0e549a72cec3b87accefc52d5cd56420251361b9
Tommy-somen <66423095+Tommy-somen@users.noreply.github.com>
2021-09-27 20:38:57 +0900

    Create README_upload.py

---
+++ README_upload.py
@@ -0,0 +1,19 @@
+"""
+terminalで
+
+#1.ディレクトリに移動
+$ cd /library/Frameworks/python.framework/Versions/3.8/bin
+
+#2.fetchを実行し、APIでローカルのsubmissionsフォルダにインプット
+./bin $ python3.8 fetch_submissions.py
+
+#3.submissionsに移動
+./bin $ cd ./submissions
+
+#4.変更内容をコミット
+./submissions $ git commit -m *
+
+#5.GithubにPush
+./submissions $ git push -u origin master
+
+"""
